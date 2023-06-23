c26b99439d64b68fb38a36a8a1ab31159c259872
lishizx <33240357+lishizx@users.noreply.github.com>
2023-06-24 02:49:50 +0800

    第一个网页

--- webApp/src/main.rs
+++ webApp/src/main.rs
@@ -1,14 +1,22 @@
 #![allow(non_snake_case)]
+#![allow(unreachable_code)]
 #[macro_use]
 extern crate nickel;
 
 use nickel::{HttpRouter, Nickel};
+use std::collections::HashMap;
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.get("/", middleware!("第一个webApp"));
-    server.get("/bar", middleware!("this is the /bar handler"));
+    server.get(
+        "/",
+        middleware! { |_, response|
+            let mut data = HashMap::new();
+            data.insert("name", "user");//传入值
+            return response.render("examples/assets/template.tpl", &data);
+        },
+    );
 
-    server.listen("127.0.0.1:8080").unwrap();
+    server.listen("127.0.0.1:6767").unwrap();
 }
---
+++ webApp/src/onePage.rs
@@ -0,0 +1,22 @@
+#![allow(non_snake_case)]
+#![allow(unreachable_code)]
+#[macro_use]
+extern crate nickel;
+
+use nickel::{HttpRouter, Nickel};
+use std::collections::HashMap;
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.get(
+        "/",
+        middleware! { |_, response|
+            let mut data = HashMap::new();
+            data.insert("name", "user");//传入值
+            return response.render("examples/assets/index.tpl", &data);
+        },
+    );
+
+    server.listen("127.0.0.1:6767").unwrap();
+}
